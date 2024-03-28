09a8d73b178685b55e676e2b455d5fa7e6494d3a
nikola1893 <mitev.nkl@gmail.com>
2024-03-28 21:25:56 +0100

    /

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,78 +5,46 @@ class Ticket < ApplicationRecord
 
   def self.print_qr_codes
     desktop_folder_path = File.expand_path("~/Desktop/alfa-tickets")
-    last(10).each_with_index do |ticket, i|
+    all.each_with_index do |ticket, i|
       qrcode = RQRCode::QRCode.new("https://alfa-events-bafea2c349ed.herokuapp.com/tickets/#{ticket.token}/enter")
       qr = qrcode.as_png(
-        color: "000",
-        shape_rendering: "crispEdges",
-        module_size: 5,
-        standalone: true,
-        use_path: true,
-        offset: 4
+        resize_gte_to: false,
+        resize_exactly_to: false,
+        offset: 7,
+        color: 'black',
+        size: 200,
+        file: nil
       )
-
-      ticket_image = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/ticket-design.png")
-      qr_image = ChunkyPNG::Image.from_blob(qr.to_s)
-      a = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/a.png")
-      b = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/b.png")
-      c = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/c.png")
-      d = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/d.png")
-      e = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/e.png")
-      f = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/f.png")
-      g = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/g.png")
-      h = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/h.png")
-      i = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/i.png")
-      j = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/j.png")
-      k = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/k.png")
-      l = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/l.png")
-      m = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/m.png")
-      n = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/n.png")
-      o = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/o.png")
-      p = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/p.png")
-      q = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/q.png")
-      r = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/r.png")
-      s = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/s.png")
-      t = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/t.png")
-      u = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/u.png")
-      v = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/v.png")
-      w = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/w.png")
-      x = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/x.png")
-      y = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/y.png")
-      z = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/z.png")
-      zero = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/0.png")
-      one = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/1.png")
-      two = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/2.png")
-      three = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/3.png")
-      four = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/4.png")
-      five = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/5.png")
-      six = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/6.png")
-      seven = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/7.png")
-      eight = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/8.png")
-      nine = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/9.png")
-
-      y_offset = (ticket_image.height - qr_image.height) / 1.25
-
-      ticket_image.compose!(qr_image, 675, y_offset)
-
-      # Load PNG images for each character
-      token_images = []
-      ticket.token.each_char do |char|
-        token_images << ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/#{char.downcase}.png")
-      end
-
-      # Compose token images onto ticket image
-      x_offset = 1575
-
-      token_images.each do |char_image|
-        ticket_image.compose!(char_image, x_offset, 40)
-        x_offset += char_image.width - 1 # Adjust for spacing between characters
-      end
   
+      qr_image = ChunkyPNG::Image.from_blob(qr.to_s)
+  
+      ticket_image = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/ticket-design.png")
+      y_offset = 278
+      x_offset = 825
+      ticket_image.compose!(qr_image, x_offset, y_offset)
+
+      token_images = {}
+      # ('a'..'z').each { |char| token_images[char] = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/#{char}.png") }
+      ('a'..'z').each do |char|
+        image = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/#{char}.png")
+        # Make the image 50% transparent
+        image.compose!(ChunkyPNG::Image.new(image.width, image.height, ChunkyPNG::Color::TRANSPARENT), 0, 0)
+        token_images[char] = image
+      end
+      ('0'..'9').each { |char| token_images[char] = ChunkyPNG::Image.from_file("#{desktop_folder_path}/assets/#{char}.png") }
+
+      x_offset = 200
+      y_offset = 20
+      ticket.token.each_char do |char|
+        ticket_image.compose!(token_images[char.downcase], x_offset, y_offset)
+        x_offset += token_images[char.downcase].width - 1
+      end
+    
       ticket_image.save("#{desktop_folder_path}/tickets/#{ticket.token}.png", :fast_rgba)
-      
+      puts "Ticket #{i + 1} saved to #{desktop_folder_path}/tickets/#{ticket.token}.png"
     end
     puts "🎉 Done"
   end
+   
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_26_155946) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_28_164339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tickets", force: :cascade do |t|
-    t.string "token", limit: 6, null: false
+    t.string "token", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
